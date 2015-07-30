0410ffd8106adcb9acf655a2c15933e321edd07b
mndvns <mail@mndvns.com>
2015-07-29 22:43:12 -0600

    better fonts styling in sidenav

--- src/root.js
+++ src/root.js
@@ -1,1 +1,12 @@
-module.exports = require('ui-kit/utils/root')();
+module.exports = require('ui-kit/utils/root')(function(res){
+  res.h1 = '36px';
+  res.h2 = '24px';
+  res.h3 = '21px';
+  res.h4 = '18px';
+  res.h5 = '16px';
+  res.h6 = '14px';
+  res.h7 = '13px';
+  res.h8 = '11px';
+  res.h9 = '8px';
+  res.h10 = '6px';
+});
